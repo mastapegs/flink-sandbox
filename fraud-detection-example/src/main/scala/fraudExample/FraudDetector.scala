f91d91fcd0cb41a40115c1db5bb5f9d7794ad6bd
Matthew Pagan <mastapegs01@gmail.com>
2024-09-03 12:15:37 -0400

    base of example compiles

---
+++ fraud-detection-example/src/main/scala/fraudExample/FraudDetector.scala
@@ -0,0 +1,31 @@
+package fraudExample
+
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
+import org.apache.flink.walkthrough.common.entity.Transaction
+import org.apache.flink.walkthrough.common.entity.Alert
+import org.apache.flink.util.Collector
+
+// class FraudDetector extends KeyedProcessFunction[Long, Transaction, Alert] {
+//   def processElement = ???
+// }
+
+class FraudDetector extends KeyedProcessFunction[Long, Transaction, Alert] {
+  @throws[Exception]
+  override def processElement(
+      transaction: Transaction,
+      context: KeyedProcessFunction[Long, Transaction, Alert]#Context,
+      collector: Collector[Alert]
+  ): Unit = {
+    val alert = new Alert()
+    alert.setId(transaction.getAccountId)
+
+    collector.collect(alert)
+  }
+}
+
+object FraudDetector {
+  private val serialVersionUID = 1L;
+  private val SMALL_AMOUNT = 1.00
+  private val LARGE_AMOUNT = 500.00
+  private val ONE_MINUTE = 60 * 1000
+}
--- fraud-detection-example/src/main/scala/fraudExample/FraudExample.scala
+++ fraud-detection-example/src/main/scala/fraudExample/FraudExample.scala
@@ -1,5 +1,24 @@
 package fraudExample
 
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.walkthrough.common.source.TransactionSource
+import org.apache.flink.walkthrough.common.sink.AlertSink
+
 object FraudExample extends App {
-  println("FraudExample")
+  val env = StreamExecutionEnvironment.getExecutionEnvironment()
+
+  val transactions = env
+    .addSource(new TransactionSource())
+    .name("transactions")
+
+  val alerts = transactions
+    .keyBy(_.getAccountId())
+    .process(new FraudDetector())
+    .name("fraud-detector")
+
+  alerts
+    .addSink(new AlertSink())
+    .name("send-alerts")
+
+  env.execute("Fraud Detection")
 }
