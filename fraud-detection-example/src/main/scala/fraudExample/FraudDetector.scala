34c51d1eda4a6b28f81a5d842f3dc324a08cd9a0
Matthew Pagan <mastapegs01@gmail.com>
2024-09-04 12:14:57 -0400

    got v1 working

--- fraud-detection-example/src/main/scala/fraudExample/FraudDetector.scala
+++ fraud-detection-example/src/main/scala/fraudExample/FraudDetector.scala
@@ -4,18 +4,35 @@ import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.walkthrough.common.entity.Transaction
 import org.apache.flink.walkthrough.common.entity.Alert
 import org.apache.flink.util.Collector
+import org.apache.flink.api.common.state.ValueState
+import org.apache.flink.api.common.functions.OpenContext
+import org.apache.flink.api.common.state.ValueStateDescriptor
+import org.apache.flink.api.common.typeinfo.Types
+import fraudExample.FraudDetector.flagState
 
 class FraudDetector extends KeyedProcessFunction[Long, Transaction, Alert] {
+  override def open(openContext: OpenContext): Unit = {
+    val flagDescriptor = new ValueStateDescriptor("flag", Types.BOOLEAN)
+    FraudDetector._flagState = getRuntimeContext().getState(flagDescriptor)
+  }
+
   @throws[Exception]
   override def processElement(
       transaction: Transaction,
       context: KeyedProcessFunction[Long, Transaction, Alert]#Context,
       collector: Collector[Alert]
   ): Unit = {
-    val alert = new Alert()
-    alert.setId(transaction.getAccountId)
+    FraudDetector.flagState.foreach { _ =>
+      if (transaction.getAmount() > FraudDetector.LARGE_AMOUNT) {
+        val alert = new Alert()
+        alert.setId(transaction.getAccountId())
+        collector.collect(alert)
+      }
+      FraudDetector._flagState.clear()
+    }
 
-    collector.collect(alert)
+    if (transaction.getAmount() < FraudDetector.SMALL_AMOUNT)
+      FraudDetector._flagState.update(true)
   }
 }
 
@@ -24,4 +41,7 @@ object FraudDetector {
   private val SMALL_AMOUNT = 1.00
   private val LARGE_AMOUNT = 500.00
   private val ONE_MINUTE = 60 * 1000
+  @transient private var _flagState: ValueState[java.lang.Boolean] = null
+  @transient private def flagState =
+    Option(_flagState.value()).map(_.booleanValue())
 }
