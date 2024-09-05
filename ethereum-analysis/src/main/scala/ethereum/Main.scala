c09510b4bc37dfdce9bdfc39d4764eebd7499249
Matthew Pagan <mastapegs01@gmail.com>
2024-09-05 13:21:01 -0400

    add URL to class constructor

--- ethereum-analysis/src/main/scala/ethereum/Main.scala
+++ ethereum-analysis/src/main/scala/ethereum/Main.scala
@@ -6,7 +6,7 @@ object Main extends App {
   val env = StreamExecutionEnvironment.getExecutionEnvironment()
 
   val sseData = env
-    .addSource(new SSESourceFunction)
+    .addSource(new SSESourceFunction("http://example.com"))
     .name("sse-data")
 
   sseData.print()
--- ethereum-analysis/src/main/scala/ethereum/SSESourceFunction.scala
+++ ethereum-analysis/src/main/scala/ethereum/SSESourceFunction.scala
@@ -2,14 +2,14 @@ package ethereum
 
 import org.apache.flink.streaming.api.functions.source.SourceFunction
 
-class SSESourceFunction extends SourceFunction[Long] {
+class SSESourceFunction(url: String) extends SourceFunction[String] {
   @volatile private var isRunning = true
   private var count = 0L
 
   def cancel(): Unit = isRunning = false
-  def run(ctx: SourceFunction.SourceContext[Long]): Unit = {
+  def run(ctx: SourceFunction.SourceContext[String]): Unit = {
     while (isRunning) {
-      ctx.collect(count)
+      ctx.collect(s"URL: ${url}, Count: ${count.toString()}")
       count += 1
     }
   }
