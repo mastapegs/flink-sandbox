4ef624cf0db9db7ca10d3bdd2ddce0e5b029a027
Matthew Pagan <mastapegs01@gmail.com>
2024-09-10 11:23:30 -0400

    create type for capturing JSON of eth head

--- ethereum-analysis/src/main/scala/ethereum/Data.scala
+++ ethereum-analysis/src/main/scala/ethereum/Data.scala
@@ -6,6 +6,31 @@ import org.apache.flink.api.scala.createTypeInformation
 case class HeadData(data: String)
 case class TxnData(data: String)
 
+case class BlockHead(
+    baseFeePerGas: String,
+    blobGasUsed: String,
+    difficulty: String,
+    excessBlobGas: String,
+    extraData: String,
+    gasLimit: Long,
+    gasUsed: Long,
+    hash: String,
+    logsBloom: String,
+    miner: String,
+    mixHash: String,
+    nonce: String,
+    number: Long,
+    parentBeaconBlockRoot: String,
+    parentHash: String,
+    receiptsRoot: String,
+    sha3Uncles: String,
+    stateRoot: String,
+    timestamp: Long,
+    totalDifficulty: String,
+    transactionsRoot: String,
+    withdrawalsRoot: String
+)
+
 trait SSEData[T] extends Serializable {
   def wrap(str: String): T
   def typeInfo: TypeInformation[T]
