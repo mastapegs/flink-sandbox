689890052151eaff76e2608977285603ebbfed0c
Matthew Pagan <mastapegs01@gmail.com>
2024-09-10 13:25:22 -0400

    update package management

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,4 +2,11 @@ import sbt._
 
 object Dependencies {
   lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
+  lazy val pekkoHttp = "org.apache.pekko" %% "pekko-http" % "1.0.1"
+  lazy val pekkoStream = "org.apache.pekko" %% "pekko-stream" % "1.0.3"
+  lazy val pekkoActor = "org.apache.pekko" %% "pekko-actor" % "1.0.3"
+  lazy val flinkStreamingScala =
+    "org.apache.flink" %% "flink-streaming-scala" % "1.20.0"
+  lazy val flinkWalkthrough =
+    "org.apache.flink" %% "flink-walkthrough-common" % "1.14.6"
 }
