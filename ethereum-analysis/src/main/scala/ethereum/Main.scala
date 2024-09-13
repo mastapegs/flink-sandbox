0f6603a35319dbf6e91eb0f3ed1a5f8453ed7611
Matthew Pagan <mastapegs01@gmail.com>
2024-09-12 22:43:22 -0400

    created table environment

--- ethereum-analysis/src/main/scala/ethereum/Main.scala
+++ ethereum-analysis/src/main/scala/ethereum/Main.scala
@@ -1,6 +1,9 @@
 package ethereum
 
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
+import org.apache.flink.table.api._
+import org.apache.flink.table.api.EnvironmentSettings
 
 object Main extends App {
   val ETHEREUM_HEAD_URL =
@@ -8,14 +11,19 @@ object Main extends App {
   val ETHEREUM_TRANSACTION_URL =
     "https://ethereum.demo.thatdot.com/mined_transactions"
 
-  val env = StreamExecutionEnvironment.getExecutionEnvironment()
+  val env = StreamExecutionEnvironment.getExecutionEnvironment
+  val tableEnv = StreamTableEnvironment.create(env);
 
   val head_data = env
-    .addSource(new SSESourceFunction[BlockHead](ETHEREUM_HEAD_URL))
+    .addSource(new SSESourceFunction[BlockHead](ETHEREUM_HEAD_URL))(
+      SSEData[BlockHead].typeInfo
+    )
     .name("head-data")
 
   val txn_data = env
-    .addSource(new SSESourceFunction[TxnData](ETHEREUM_TRANSACTION_URL))
+    .addSource(new SSESourceFunction[TxnData](ETHEREUM_TRANSACTION_URL))(
+      SSEData[TxnData].typeInfo
+    )
     .name("txn-data")
 
   head_data.print("Head Data")
